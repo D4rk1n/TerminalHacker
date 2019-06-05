97c05261a6be660938c334bfa78390ddfa8ae29d
Darkin <darkinb255@gmail.com>
2019-06-05 22:04:02 +0200

    animations

--- Terminal Hacker/Program.cs
+++ Terminal Hacker/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Terminal_Hacker
@@ -11,11 +12,28 @@ namespace Terminal_Hacker
  
         int Diff;
         Screen Mode = Screen.Menu;
-        public  void MainMenu()
+        public async Task MainMenuAsync()
         {
             THName();
+            string Progresbar = "Terminal Hacker";
+            var title = "";
+           
+           
+                for (int i = 0; i < Progresbar.Length; i++)
+                {
+                    title += Progresbar[i];
+                    Console.Title = title;
+                Thread.Sleep(100);
+
+                }
+                title = "";
+           
+ 
             bool next = false;
             Console.WriteLine("Choose a Terminal to Be Hacked");
+            Console.WriteLine("1)");
+            Console.WriteLine("2)");
+            Console.WriteLine("3)");
 
             while(!next)
             {
@@ -25,41 +43,63 @@ namespace Terminal_Hacker
                     Diff = 1;
                     next = true;
                     Mode = Screen.Password;
+                    Console.ForegroundColor = ConsoleColor.Green;
                 }
                 if (choice == "2")
                 {
                     Diff = 2;
                     next = true;
                     Mode = Screen.Password;
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                }
+                if (choice == "3")
+                {
+                    Diff = 3;
+                    next = true;
+                    Mode = Screen.Password;
+                    Console.ForegroundColor = ConsoleColor.Red;
                 }
 
             }
+            Console.Clear();
            
         }
         private static void THName()
         {
-            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("  _______ ______ _____  __  __ _____ _   _          _ ");
+            Thread.Sleep(100);
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
             Console.WriteLine(" |__   __|  ____|  __ \\|  \\/  |_   _| \\ | |   /\\   | | ");
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Thread.Sleep(100);
             Console.WriteLine("    | |  | |__  | |__) | \\  / | | | |  \\| |  /  \\  | | ");
+            Thread.Sleep(100);
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("    | |  |  __| |  _  /| |\\/| | | | | . ` | / /\\ \\ | | ");
-            Console.ForegroundColor = ConsoleColor.Red;
+            Thread.Sleep(100);
             Console.WriteLine("    | |  | |____| | \\ \\| |  | |_| |_| |\\  |/ ____ \\| |____ ");
+            Thread.Sleep(100);
             Console.WriteLine("    |_|  |______|_|  \\_\\_|  |_|_____|_| \\_/_/    \\_\\______|");
+            Thread.Sleep(100);
             Console.WriteLine("          _    _          _____ _  ________ _____          ");
+            Thread.Sleep(100);
             Console.WriteLine("         | |  | |   /\\   / ____| |/ /  ____|  __ \\         ");
-            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Thread.Sleep(100);
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
             Console.WriteLine("         | |__| |  /  \\ | |    | ' /| |__  | |__) |");
+            Thread.Sleep(100);
             Console.WriteLine("         |  __  | / /\\ \\| |    |  < |  __| |  _  /");
+            Thread.Sleep(100);
             Console.WriteLine("         | |  | |/ ____ \\ |____| . \\| |____| | \\ \\ ");
+            Thread.Sleep(100);
             Console.WriteLine("         |_|  |_/_/    \\_\\_____|_|\\_\\______|_|  \\_\\ \n\n");
             Console.ResetColor();
         }
         public void CrackPass()
         {
-            Console.Clear();
             Console.Beep();
+            Console.WriteLine("You Have Chosen Diffculty : {0}", Diff);
+            
             Mode = Screen.Win;
         }
 
@@ -72,7 +112,7 @@ namespace Terminal_Hacker
                 switch (p.Mode)
                 {
                     case Screen.Menu:
-                        p.MainMenu();
+                        p.MainMenuAsync();
                         break;
                     case Screen.Password:
                         p.CrackPass();
